c766227cd84803f867c6f9fdee8efd9900ef9c59
Javier <javierggjavier@gmail.com>
2021-07-29 13:39:13 +0100

    Close resources automatically with Try with Resources

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,19 +9,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:/Volumes/Production/Courses/Programs/JavaPrograms/TestDB/testjava.db");
-//            Statement statement = conn.createStatement()) {
-//            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
-        try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Rosana&Javier\\databases\\testjava.db");
-            Statement statement = conn.createStatement();
+        /*
+        Connecting and creating a local data base on Windows computer and
+        Try with Resources to close automatically our resources
+        */
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Rosana&Javier\\databases\\testjava.db");
+             Statement statement = conn.createStatement();) {
+
             statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
 
-            statement.close();
-            conn.close();
-
-//            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\databases\\testjava.db");
-//            Class.forName("org.sql.JDBC");
 
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
